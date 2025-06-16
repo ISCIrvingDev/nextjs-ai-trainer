aaf7f5b2473a352d6c4f262119268c427ad62b5c
Irving Salazar <ISCIrvingDev@gmail.com>
2025-06-16 16:36:15 -0600

    TODO: Crear el handler para el "user.updated"

--- convex/http.ts
+++ convex/http.ts
@@ -119,7 +119,29 @@ http.route({
       }
     }
 
-    // * TODO: Crear el handler para el "user.updated"
+    if (eventType === "user.updated") {
+      const { id, first_name, last_name, image_url, email_addresses } =
+        evt.data;
+
+      const email = email_addresses[0].email_address;
+
+      const name = `${first_name || ""} ${last_name || ""}}`.trim();
+
+      try {
+        await ctx.runMutation(api.users.updateUser, {
+          email,
+          name,
+          image: image_url,
+          clerkId: id,
+        });
+      } catch (error) {
+        const msn = "Error updating user";
+
+        console.error(`${msn}: `, error);
+
+        return new Response(msn);
+      }
+    }
 
     return new Response("Webhooks processed successfully", { status: 200 });
   }),
